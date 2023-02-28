e8351b1b0a399c303cf7d839d500e0a7e8934ddc
Erick Salas <erick.salas@gft.com>
2023-02-27 20:08:42 -0600

    agregar comentario

--- carrito_clase.py
+++ carrito_clase.py
@@ -1,5 +1,19 @@
+from funciones_utiles import crear_id
 class Carrito(dict):
-    # coloque aqui la logica
-    # que considere necesario
-    # atributos y metodos
-    pass
+    contador_de_carrito = 0
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.identificador = crear_id()
+        self.lista_articulos = []
+        self.incrementar_carrito()
+
+
+    @classmethod
+    def incrementar_carrito(cls):
+        cls.contador_de_carrito += 1
+
+    def pagar(self):
+        print('pagando')
+
+    def agregar_item(self, item):
+        self.lista_articulos.append(item)
--- control.py
+++ control.py
@@ -1,4 +1,45 @@
+
 from generador_inventario_clientes import obtener_inventario_clientes
+from carrito_clase import Carrito
+from funciones_utiles import crear_id
+
+# inventario de carritos
+inventario_de_carritos = {}
+
+def obtener_carritos(cedula_cliente):
+    lista_carritos = []
+    for carrito in inventario_de_carritos.values():
+        cedula = carrito.get('cliente').get('cedula')
+        if cedula_cliente == cedula:
+            lista_carritos.append(carrito)
+    return lista_carritos
+
+def agregar_carrito_al_inventario(cedula_cliente):
+    id_nuevo_carrito = crear_id()
+    # quien es el cliente?
+    # buscar el cliente -> en el inventario de clientes
+    inventario_clientes = obtener_inventario_clientes()
+    el_cliente = inventario_clientes.get(cedula_cliente)
+    inventario_de_carritos.update(
+        {
+            id_nuevo_carrito: Carrito(cliente=el_cliente) # aqui va los parametros de la calse carrito
+        }
+    )
+
+def obtener_id_productos_carritos(lista_carritos):
+    lista_ids_productos = []
+    for carrito in lista_carritos:
+        # falta agregar los nombres de los productos
+        lista_ids_productos.append(carrito.identificador)
+    return lista_ids_productos
+
+def obtener_carrito__por_id(id):
+    mi_carrito = None
+    for carrito in inventario_de_carritos.values():
+        if id == carrito.identificador:
+            mi_carrito = carrito
+    return mi_carrito
+
 
 def control_flujo():
     # coloque aqui la logica
